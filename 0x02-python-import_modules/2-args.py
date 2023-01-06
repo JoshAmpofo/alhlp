b50692131546ddae20522e36fbd85de59c323e57
JoshAmpofo <joshampofo92@gmail.com>
2023-01-06 14:47:42 +0000

    Alternative print solution

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-import sys
+
 if __name__ == "__main__":
-    # printing number of arguments
-    num_args = len(sys.argv) - 1
+    import sys
 
-    if num_args == 0:
-        print("{} arguments.".format(num_args))
-    elif num_args == 1:
-        print("{} argument: ".format(num_args))
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print("{}: arguments:".format(num_args))
-
-    # printing arguments
-    for pos, args in enumerate(sys.argv[1:]):
-        print("{}: {}".format(pos+1, args))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
