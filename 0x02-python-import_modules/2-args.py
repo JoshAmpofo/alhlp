69edf9da6a3e3c7f04e364e0b5ed47b3023e5565
JoshAmpofo <joshampofo92@gmail.com>
2023-01-06 14:00:31 +0000

    Print cmd arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    # printing number of arguments
+    num_args = len(sys.argv) - 1
+
+    if num_args == 0:
+        print("{} arguments.".format(num_args))
+    elif num_args == 1:
+        print("{} argument: ".format(num_args))
+    else:
+        print("{} arguments:".format(num_args))
+
+    # printing arguments
+    for pos, args in enumerate(sys.argv[1:]):
+        print("{}: {}".format(pos+1, args))
