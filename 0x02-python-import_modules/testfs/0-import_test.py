b8de19ba113105470ccb4a362d6735fa6963a1fc
JoshAmpofo <joshampofo92@gmail.com>
2023-01-06 17:09:00 +0000

    Add new test

--- 0x02-python-import_modules/testfs/0-import_test.py
+++ 0x02-python-import_modules/testfs/0-import_test.py
@@ -1,6 +1,9 @@
+# TESTS FOR IMPORT EXECUTION
+
 #__import__("0-add")
 #__import__("1-calculation")
 #__import__("2-args")
 #__import__("3-infinite_add")
 #__import__("4-hidden_discovery")
-__import__("5-variable_load")
+#__import__("5-variable_load")
+__import__("100-my_calculator")
