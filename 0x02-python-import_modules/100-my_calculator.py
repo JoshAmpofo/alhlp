dda3d46c10fd4dd0057ccf32a6aa6417edb22c40
JoshAmpofo <joshampofo92@gmail.com>
2023-01-06 17:06:09 +0000

    Fix req error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,7 +5,7 @@ from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
 
     # Check if there are three command line arguments
-    if len(sys.argv) -1 != 3:
+    if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
