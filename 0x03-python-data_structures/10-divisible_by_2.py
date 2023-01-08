ea358af6398a4411b27ae1579268d06f01d4722e
JoshAmpofo <joshampofo92@gmail.com>
2023-01-08 14:03:01 +0000

    Minor code addition

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     multiple = []
     for num in range(len(my_list)):
-        if num % 2 == 0:
+        if my_list[num] % 2 == 0:
             multiple.append(True)
         else:
             multiple.append(False)
