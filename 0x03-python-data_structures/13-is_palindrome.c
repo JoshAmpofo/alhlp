097a3a2e0b791107312fb795173904b70bfd0d63
JoshAmpofo <joshampofo92@gmail.com>
2023-01-08 15:38:24 +0000

    Improve efficiency

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,19 +1,51 @@
 #include "lists.h"
+/**
+ * is_palindrome_helper - compare value of current node with value of node
+ * at end of linked list
+ * @head: pointer to beginning of node
+ * @length: size of node
+ * @node: node
+ * Return: 0.
+ */
+int is_palindrome_helper(listint_t *head, int length, listint_t **node)
+{
+	if (head == NULL || length <= 0)
+	{
+		return (1);
+	}
+	else if (length == 1)
+	{
+		*node = head->next;
+		return (1);
+	}
 
+	if (!(is_palindrome_helper(head->next, length - 2, node)))
+	{
+		return (0);
+	}
+	if ((*node)->n == head->n)
+	{
+		*node = (*node)->next;
+		return (1);
+	}
+	return (1);
+}
 /**
  * is_palindrome - check if singly linked list is a palindrome
  * @head: pointer to beginning of node
  *
  * Return: 0 if not palindrome, 1 if it is
  */
+
 int is_palindrome(listint_t **head)
 {
-	int i, length, top;
-	int *stack;
-	listint_t *current;
+	listint_t *current, *node;
+	int length;
 
 	if (*head == NULL)
+	{
 		return (1);
+	}
 
 	current = *head;
 	length = 0;
@@ -23,25 +55,7 @@ int is_palindrome(listint_t **head)
 		current = current->next;
 		length++;
 	}
+	node = *head;
 
-	current = *head;
-
-	stack = malloc(sizeof(int) * length);
-	top = -1;
-
-	for (i = 0; i < length / 2; i++)
-	{
-		stack[++top] = current->n;
-		current = current->next;
-	}
-
-	if (length % 2 != 0)
-		current = current->next;
-	for (i = 0; i < length / 2; i++)
-	{
-		if (stack[top--] != current->n)
-			return (0);
-		current = current->next;
-	}
-	return (1);
+	return (is_palindrome_helper(*head, length, &node));
 }
