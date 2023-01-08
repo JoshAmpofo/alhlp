47da0391395a354f6d8aa70a4766ebb6d1576662
JoshAmpofo <joshampofo92@gmail.com>
2023-01-08 11:15:45 +0000

    Refactor function

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
-    return new_string
+    new_string = [char for char in my_string if char != 'c' and char != 'C']
+    return "".join(new_string)
