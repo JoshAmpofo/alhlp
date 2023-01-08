d91bef72668d680dbc2dcd05225ad24f0e6c441c
JoshAmpofo <joshampofo92@gmail.com>
2023-01-08 15:45:40 +0000

    Major Refactor

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,61 +1,72 @@
 #include "lists.h"
-/**
- * is_palindrome_helper - compare value of current node with value of node
- * at end of linked list
- * @head: pointer to beginning of node
- * @length: size of node
- * @node: node
- * Return: 0.
- */
-int is_palindrome_helper(listint_t *head, int length, listint_t **node)
-{
-	if (head == NULL || length <= 0)
-	{
-		return (1);
-	}
-	else if (length == 1)
-	{
-		*node = head->next;
-		return (1);
-	}
 
-	if (!(is_palindrome_helper(head->next, length - 2, node)))
-	{
-		return (0);
-	}
-	if ((*node)->n == head->n)
-	{
-		*node = (*node)->next;
-		return (1);
-	}
-	return (1);
-}
+listint_t *reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+
 /**
- * is_palindrome - check if singly linked list is a palindrome
- * @head: pointer to beginning of node
+ * reverse_listint - Reverses a singly-linked listint_t list.
+ * @head: A pointer to the starting node of the list to reverse.
  *
- * Return: 0 if not palindrome, 1 if it is
+ * Return: A pointer to the head of the reversed list.
  */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
 
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks if a singly linked list is a palindrome.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: If the linked list is not a palindrome - 0.
+ *         If the linked list is a palindrome - 1.
+ */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current, *node;
-	int length;
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-	if (*head == NULL)
-	{
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	}
 
-	current = *head;
-	length = 0;
-
-	while (current != NULL)
+	tmp = *head;
+	while (tmp)
 	{
-		current = current->next;
-		length++;
+		size++;
+		tmp = tmp->next;
 	}
-	node = *head;
 
-	return (is_palindrome_helper(*head, length, &node));
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
+	{
+		if (tmp->n != rev->n)
+			return (0);
+		tmp = tmp->next;
+		rev = rev->next;
+	}
+	reverse_listint(&mid);
+
+	return (1);
 }
