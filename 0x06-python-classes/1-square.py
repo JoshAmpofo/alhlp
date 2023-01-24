9d043c77dd71c79050ff311fd1da20d9c63a92dc
JoshAmpofo <joshampofo92@gmail.com>
2023-01-24 16:01:25 +0000

    Initialize class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module that defines a class Square"""
+
+
+class Square:
+    """create an instance of the class Square"""
+    def __init__(self, size):
+        """Initialize attributes of the class Square.
+
+        Args:
+        __size (int): private instance of the class Square.
+        Determines the size of the class Square.
+        """
+        self.__size = size
