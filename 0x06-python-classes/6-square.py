de774c37211d1117a694422bf42ef43bb28b181a
JoshAmpofo <joshampofo92@gmail.com>
2023-01-24 20:22:59 +0000

    Fix bug

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -14,8 +14,8 @@ class Square:
         __position (int, int): private instance of the class Square.
         Determines the coordinates of the class Square
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -38,8 +38,8 @@ class Square:
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or len(value) != 2 or
-                not all(isinstance(i, int) for i in value)
-                or not all(i >= 0 for i in value)):
+                not all(isinstance(num, int) for num in value)
+                or not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
