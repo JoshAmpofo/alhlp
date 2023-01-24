71b0c9598e428d26168e9df534335c81ba0686dc
JoshAmpofo <joshampofo92@gmail.com>
2023-01-24 15:54:11 +0000

    Fix req error

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This module is class function that defines a class Square"""
+"""Module that defines a class Square"""
+
 
 class Square:
     """create an empty instance of the class Square"""
