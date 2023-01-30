6b7d31481d74ba10bae2c98f71609afadc32889b
JoshAmpofo <joshampofo92@gmail.com>
2023-01-30 08:51:52 +0000

    Make file executable

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Create a class Rectangle that defines attributes of
+the geometrical figure
+"""
+
+
+class Rectangle():
+    """define an empty class rectangle"""
+    pass
