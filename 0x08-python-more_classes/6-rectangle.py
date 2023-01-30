1e699da13bba1138c75b3458d672e68009dc826d
JoshAmpofo <joshampofo92@gmail.com>
2023-01-30 13:08:44 +0000

    Fix req error

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle():
         """
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -88,4 +88,4 @@ class Rectangle():
     def __del__(self):
         """delete an instance in Rectangle"""
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+        type(self).number_of_instances -= 1
