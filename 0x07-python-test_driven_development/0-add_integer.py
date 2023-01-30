6dabdfbc7ff2a63184450177146579a941ad217e
JoshAmpofo <joshampofo92@gmail.com>
2023-01-30 20:08:13 +0000

    Add two integers

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is the "0-add_integer" module.
+
+The modules supplies only one function, add_integer(a, b).
+"""
+
+
+def add_integer(a, b=98):
+    """add two integers"""
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    if type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    return a + b
