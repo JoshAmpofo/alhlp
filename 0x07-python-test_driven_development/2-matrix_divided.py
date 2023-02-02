3c8ec745fa0158b4e26df7123ae932d323acff2b
JoshAmpofo <joshampofo92@gmail.com>
2023-02-02 08:40:14 +0000

    Fix merge conflicts

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,13 +13,9 @@ def matrix_divided(matrix, div):
         or not all(isinstance(row, list) for row in matrix)
         or not all(isinstance(element, int) or isinstance(element, float)
                    for element in [item for row in matrix for item in row])):
-<<<<<<< HEAD
         raise TypeError("matrix must be a matrix (list of lists)"
                         " of integers/floats")
-=======
-        raise TypeError("matrix must be a matrix(lists of lists)"
-                        "of integers/floats")
->>>>>>> refs/remotes/origin/main
+
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, int) and not isinstance(div, float):
