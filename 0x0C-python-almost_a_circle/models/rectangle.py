114b0cdbe6eb8e53d02e4b011b2d129af0893652
JoshAmpofo <joshampofo92@gmail.com>
2023-02-10 12:10:12 +0000

    Refactor __str__ method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -125,8 +125,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """canonical string representation of object Rectangle"""
-        return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(self.id,
-                                                                 self.__x,
-                                                                 self.__y,
-                                                                 self.__width,
-                                                                 self.__height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.__x,
+                                                       self.__y,
+                                                       self.__width,
+                                                       self.__height)
