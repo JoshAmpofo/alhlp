95160657fab8708b3348f5a3c7c8d741f828ad72
JoshAmpofo <joshampofo92@gmail.com>
2023-02-11 22:17:21 +0000

    Add task test files

---
+++ 0x0C-python-almost_a_circle/task_test/0-main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" 0-main """
+from models.base import Base
+
+if __name__ == "__main__":
+
+#    b1 = Base()
+#    print(b1.id)
+
+#    b2 = Base()
+#    print(b2.id)
+
+#    b3 = Base()
+#    print(b3.id)
+
+#    b4 = Base(12)
+#    print(b4.id)
+
+#    b5 = Base()
+#    print(b5.id)
+
+#    b6 = Base(None)
+#    print(b6.id)
+
+#    b7 = Base(12.6)
+#    print(b7.id)
+
+#    b8 = Base({"a": 1, 'b': 2})
+#    print(b8.id)
+
+#    b9 = Base(float('NaN'))
+#    print(b9.id)
+
+#    b10 = Base(frozenset({1, 2, 3}))
+#    print(b10.id)
+
+#    b11 = Base(float('inf'))
+#    print(b11.id)
+
+#    b12 = Base('I am not an int')
+#    print(b12.id)
+
+#    b13 = Base(True)
+#    print(b13.id)
+
+#    b14 = Base(range(5))
+#    print(b14.id)
+
+#    b15 = Base(1, 2)
+#    print(b15.id)
+
+#    b16 = Base()
+#    b16.id = 15
+#    print(b16.id)
+
+    b17 = Base(b'Bytemode')
+    print(b17.id)
+
+    b18 = Base(complex(5))
+    print(b18.id)
