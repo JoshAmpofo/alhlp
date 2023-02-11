635ef75c69a5cc59bec2389509d3fc594e2342ba
JoshAmpofo <joshampofo92@gmail.com>
2023-02-11 21:12:14 +0000

    Return JSON str rep of dict

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Define class Base"""
 
+import json
+
 
 class Base:
     """creates a Base class"""
@@ -17,3 +19,15 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list dictionaries
+
+        Args:
+            list_dictionaries (list): list of dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
