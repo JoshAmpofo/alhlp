dc85fd0af4881a6c1dc19b66845c666d68a9b435
JoshAmpofo <joshampofo92@gmail.com>
2023-03-14 14:42:22 +0000

    Insert values into DB table

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,6 @@
+-- This script will add values to rows in the `first_table` 
+-- The row names are `id` and `name`
+-- The DB name will be supplid as and arg of the mysql command
+
+
+INSERT INTO first_table (id, name) VALUES ('89', 'Best School');
