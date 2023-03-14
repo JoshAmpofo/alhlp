f6b4b5a50eb6249f44bf27403129dfeb3af63218
JoshAmpofo <joshampofo92@gmail.com>
2023-03-14 14:58:34 +0000

    Count entries in a table

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- This script will display the number of records in a selected column in db
+-- The db name will be supplied as an arg of the mysql command
+
+
+SELECT COUNT(*) FROM first_table WHERE id=89;
