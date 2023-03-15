73f8131cf848fd7593d56318b9afe2659edabe2c
JoshAmpofo <joshampofo92@gmail.com>
2023-03-15 11:32:44 +0000

    Create new user in db

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- This script will create a user `user_0d_1`
+-- User will have all privileges
+-- User password is 'user_0d_1_pwd'
+-- Script should not fail if user already exists
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
