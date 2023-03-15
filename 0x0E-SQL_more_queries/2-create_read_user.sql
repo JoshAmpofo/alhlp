4f8e974187e488a3a428afde15e167cc4779a87c
JoshAmpofo <joshampofo92@gmail.com>
2023-03-15 11:44:20 +0000

    Grant SELECT perms to user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- This script will create a new db `hbtn_0d_2` and user `user_0d_2`
+-- User should have only SELECT privilege in db
+-- User password should be set to 'user_0d_2_pwd'
+-- Script should not fail if db and user already exists
+
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_02.* TO 'user_0d_2'@'localhost';
