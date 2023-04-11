2db03db3b3ae2a16c0ac4a3c573f9665c2b49b31
JoshAmpofo <joshampofo92@gmail.com>
2023-04-11 21:38:24 +0000

    Fix vagrant file corruption error

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,10 @@
 
 /* This script will print 'x' times "C is fun"
   'x' is the first argument of the script
-  If first argument can't be converted to an integer, print "Missing number of occurences"
+  If the argument can't be converted to an integer, print
+  "Missing number of occurrences".
   You can only use two console.log
-  You must use a loop (while, for, etc)
+  You must use a loop (while, for, etc.)
 */
 
 const input = process.argv[2];
