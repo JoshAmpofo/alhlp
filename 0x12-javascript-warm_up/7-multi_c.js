58665837d54a861e09a45e7745dbaafbbb201e30
JoshAmpofo <joshampofo92@gmail.com>
2023-04-11 21:13:23 +0000

    Print string by stipulated number

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+/* This script will print 'x' times "C is fun"
+  'x' is the first argument of the script
+  If first argument can't be converted to an integer, print "Missing number of occurences"
+  You can only use two console.log
+  You must use a loop (while, for, etc)
+*/
+
+const input = process.argv[2];
+const parsedInpt = parseInt(input);
+
+if (isNaN(parsedInpt)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parsedInpt; i++) {
+    console.log('C is fun');
+  }
+}
