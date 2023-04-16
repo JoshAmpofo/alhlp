93b82df229e8d504f0413dad8d6cb8298c58623a
JoshAmpofo <joshampofo92@gmail.com>
2023-04-16 07:47:42 +0000

    Filter states by leading character

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This script will search for entries beginning with a specified letter"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # connect to database
+    try:
+        db = MySQLdb.connect(host="localhost",
+                             user=mysql_username,
+                             passwd=mysql_password,
+                             db=database_name,
+                             port=3306)
+        cur = db.cursor()
+    except MySQLdb.Error as e:
+        print(f"MySQLdb Error {e.args[0]}: {e.args[1]}")
+        sys.exit(1)
+
+    # execute search query
+    try:
+        cur.execute("SELECT * FROM states WHERE name LIKE 'N%'\
+                    ORDER BY states.id ASC")
+        states = cur.fetchall()
+    except MySQLdb.Error as e:
+        print(f"MySQLdb Error {e.args[0]}: {e.args[1]}")
+
+    # print results
+    for state in states:
+        print(state)
+
+    # clean up
+    cur.close()
+    db.close()
