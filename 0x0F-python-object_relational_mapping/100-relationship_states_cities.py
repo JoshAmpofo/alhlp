987737ea240c0a71404e1bc3d68fa017a8c46c5a
JoshAmpofo <joshampofo92@gmail.com>
2023-04-17 09:40:05 +0000

    Add new city and state to db

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This script will create the State obj 'California' with the city
+'San Francisco' from the db `hbtn_0e_100_usa`
+Use the 'cities' relationship for all 'State' objects
+"""
+
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    # create new engine and session
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, db_name),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # create or insert a new State and City object
+    new_state = State(name='California')
+    new_city = City(name='San Francisco')
+    new_state.cities.append(new_city)
+
+    session.add(new_state)
+    session.add(new_city)
+    session.commit()
+    session.close()
